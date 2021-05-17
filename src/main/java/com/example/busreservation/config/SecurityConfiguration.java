c8b300513a4575e4d7575d9cd9297bf1eba93e6c
Cho Mu Hyeon <cmhxone@outlook.kr>
2021-05-17 16:06:04 +0900

    H2 Console Enabled

--- src/main/java/com/example/busreservation/config/SecurityConfiguration.java
+++ src/main/java/com/example/busreservation/config/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.busreservation.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +13,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource datasource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("{noop}Comtec123!").authorities("ROLE_USER");
+		auth
+			.inMemoryAuthentication()
+			.withUser("driver")
+			.password("{noop}Comtec123!")
+			.authorities("ROLE_DRIVER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").access("hasRole('ROLE_USER')").and().formLogin();
+		http
+			.authorizeRequests()
+				.antMatchers("/")
+				.permitAll()
+				.antMatchers("/driver")
+				.access("hasRole('ROLE_DRIVER')")
+			.and()
+			.csrf()
+				.ignoringAntMatchers("/h2-console/**")
+			.and()
+			.headers()
+				.frameOptions()
+				.disable()
+			.and()
+			.formLogin();
 	}
 
 }
---
+++ src/main/java/com/example/busreservation/dto/Driver.java
@@ -0,0 +1,10 @@
+package com.example.busreservation.dto;
+
+import lombok.Data;
+
+@Data
+public class Driver {
+
+	private int id;
+	private String name;
+}
