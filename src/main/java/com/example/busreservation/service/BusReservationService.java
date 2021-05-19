e2b1ee2da264f16c30c7e55a5b102221694c77ab
cmhxone <cmhxone@outlook.kr>
2021-05-19 23:24:06 +0900

    Remove Temporary Files

--- src/main/java/com/example/busreservation/service/BusReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.busreservation.service;
-
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
-public interface BusReservationService {
-    public Map<String, String> showTables();
-}
