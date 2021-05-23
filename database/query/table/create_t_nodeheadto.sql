2ea381eb4295843300088018eb9fa9d6aca7ce14
cmhxone <cmhxone@outlook.kr>
2021-05-23 23:26:31 +0900

    Remove FK_Constraint

--- database/query/table/create_t_nodeheadto.sql
+++ database/query/table/create_t_nodeheadto.sql
@@ -1,6 +1,4 @@
 CREATE TABLE t_nodeheadto (
     nodeno VARCHAR(10) NOT NULL,
-    headto VARCHAR(30),
-
-    CONSTRAINT fk_t_nodeheadto_nodeid FOREIGN KEY(nodeid) REFERENCES t_node(nodeid)
+    headto VARCHAR(30)
 );
