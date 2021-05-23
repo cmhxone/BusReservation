6ef4812ddf476561f85a8f8e6e01eea336e86f0a
cmhxone <cmhxone@outlook.kr>
2021-05-23 21:23:44 +0900

    Modify "NodeHeadTo" table

--- database/query/table/create_t_nodeheadto.sql
+++ database/query/table/create_t_nodeheadto.sql
@@ -1,6 +1,6 @@
 CREATE TABLE t_nodeheadto (
     nodeno VARCHAR(10) NOT NULL,
-    headto VARCHAR(30) DEFAULT '',
+    headto VARCHAR(30),
 
     CONSTRAINT fk_t_nodeheadto_nodeid FOREIGN KEY(nodeid) REFERENCES t_node(nodeid)
 );
