2dc2c6e56b5d95d2299a2f8188d77f3f1ad2ac37
Cho Mu Hyeon <cmhxone@outlook.kr>
2021-05-24 09:29:38 +0900

    Refactor PassengerController, passanger.html

--- src/main/java/com/example/busreservation/controller/PassangerController.java
+++ src/main/java/com/example/busreservation/controller/PassangerController.java
@@ -1,8 +1,9 @@
 package com.example.busreservation.controller;
 
-import com.example.busreservation.dto.Node;
-import com.example.busreservation.service.NodeHeadToService;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.example.busreservation.dto.Node;
+import com.example.busreservation.service.NodeHeadToService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequestMapping("/passenger")
@@ -19,12 +23,13 @@ public class PassangerController {
 
     @Autowired
     NodeHeadToService nodeHeadToService;
-
+    
     @GetMapping("")
     public String defaultPage(@RequestParam(name = "page", required = true, defaultValue = "0") int page,
                               @RequestParam(name = "searchNodeName", required = false, defaultValue = "") String searchNodeName,
+                              HttpServletRequest http,
                               Model model) {
-
+    	
         int prevpage, nextpage;
         // 페이지 한계 지정
         if (page < 0) {
