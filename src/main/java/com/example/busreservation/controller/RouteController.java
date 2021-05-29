3e9ef575638fbf413624de8bd4cb342bac66fed5
cmhxone <cmhxone@outlook.kr>
2021-05-30 00:31:44 +0900

    Add Route Controller

---
+++ src/main/java/com/example/busreservation/controller/RouteController.java
@@ -0,0 +1,23 @@
+package com.example.busreservation.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RequestMapping("/route")
+@Controller
+public class RouteController {
+
+    @GetMapping("/{routeid}/{nodeid}")
+    public String getRouteInfo(HttpServletRequest http,
+                               @PathVariable(name = "routeid", required = true) String routeid,
+                               @PathVariable(name = "nodeid", required = true) String nodeid) {
+
+        return "redirect:/node/" + nodeid;
+    }
+}
