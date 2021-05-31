23596643f1957bda6211d860d5719d777f587f1f
cmhxone <cmhxone@outlook.kr>
2021-05-31 22:19:07 +0900

    Reservation Stored in DB

--- src/main/java/com/example/busreservation/serviceImpl/ReservationServiceImpl.java
+++ src/main/java/com/example/busreservation/serviceImpl/ReservationServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.busreservation.serviceImpl;
 
 import com.example.busreservation.dto.Reservation;
 import com.example.busreservation.event.ReservedEvent;
+import com.example.busreservation.mapper.ReservationMapper;
 import com.example.busreservation.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
@@ -11,11 +12,16 @@ import org.springframework.stereotype.Service;
 public class ReservationServiceImpl implements ReservationService {
 
     @Autowired
+    ReservationMapper reservationMapper;
+
+    @Autowired
     ApplicationEventPublisher publisher;
 
     @Override
     public boolean reserve(String nodeid, String routeid) {
 
+        reservationMapper.reserve(nodeid, routeid);
+        
         Reservation reservation = new Reservation(nodeid, routeid);
 
         publisher.publishEvent(new ReservedEvent(reservation));
